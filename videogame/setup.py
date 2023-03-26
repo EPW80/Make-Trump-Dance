01d234177d2e67ce4d682cf7da6772f7068edb4b
Erik Williams <epwilliams@csu.fullerton.edu>
2023-03-25 22:03:53 -0700

    building setup.py

--- videogame/setup.py
+++ videogame/setup.py
@@ -17,11 +17,24 @@ setup_info = {
     "name": "videogame",
     "version": "0.1",
     "description": "A package to support writing games with PyGame",
-    # TODO: Optional, add more information to the setup.py script
-    # "long_description": open("README.md").read(),
+    "long_description": open("README.md").read(),
+    "long_description_content_type": "text/markdown",
     "author": "EPW80",
     "author_email": "epwilliams@csu.fullerton.edu",
-    # "url": "https://some.url/somehwere/maybe/github",
+    "url": "https://github.com/cpsc-spring-2023/cpsc-386-04-scene-EPW80",
+    "license": "MIT",
+    "classifiers": [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    "packages": ["videogame"],
+    "install_requires": [
+        "pygame",
+        # Add any additional dependencies your project requires
+    ],
 }
 
 setup(**setup_info)
